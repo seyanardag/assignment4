095107c8eca191cc259e7b1de9fcb1c8a1f9df24
Seyf <seyanardag@gmail.com>
2022-04-16 14:04:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+app.options('*', cors());
+
+const PORT = process.env.PORT || 5000
+
+var data = require("./app.json");
+
+app.get("/", function(req, res) {
+  res.json(data);
+});
+
+app.listen(PORT, function() {
+  console.log("Server running on port 5000...");
+});
